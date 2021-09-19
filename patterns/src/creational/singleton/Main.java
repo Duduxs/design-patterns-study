c2c6781761680fc98f00bb398e5318589f8e1a19
Eduardo <eduarddoj1@gmail.com>
2021-09-19 01:20:28 -0300

    main 	:rocket: Adicao de exemplo do Singleton

---
+++ patterns/src/creational/singleton/Main.java
@@ -0,0 +1,22 @@
+package creational.singleton;
+
+import creational.singleton.db.MyDatabaseClassic;
+import creational.singleton.db.User;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var myDatabaseClassic = MyDatabaseClassic.getInstance();
+        var myDatabaseClassic2 = MyDatabaseClassic.getInstance();
+
+
+        myDatabaseClassic.add(new User("Eduardo"));
+        myDatabaseClassic.add(new User("João"));
+        myDatabaseClassic.add(new User("Maria"));
+
+        myDatabaseClassic.show();
+
+        System.out.println(myDatabaseClassic == myDatabaseClassic2);
+
+    }
+}
---
+++ patterns/src/creational/singleton/db/User.java
@@ -0,0 +1,13 @@
+package creational.singleton.db;
+
+public class User {
+    private String name;
+
+    public User(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
