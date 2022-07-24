2bc5800a5d21111fb2e2aa7cc212effb15ea8300
Eduardo José <duduxss3@gmail.com>
2022-07-23 23:29:38 -0300

    main
    	:art: Making the Director class comment better

--- patterns/src/patterns/creational/builder/examples/third/classes/Director.java
+++ patterns/src/patterns/creational/builder/examples/third/classes/Director.java
@@ -3,9 +3,11 @@ package patterns.creational.builder.examples.third.classes;
 import patterns.creational.builder.examples.third.interfaces.MealBuilderProtocol;
 
 /**
- * O diretor no pattern Builder define a ordem de criação dos builders.
- * Encapsulando ainda mais a estrutura dos builders e ajudando a criação de elementos do lado
- * do código cliente (código da aplicação).
+ * O diretor no pattern Builder define a ordem e a forma de criação dos builders.
+ * Ou seja, qual será a ordem de cada método chain e também quais serão os métodos de determinado builder chamado.
+ * Ele ajuda ainda mais a encapsular a estrutura dos builders e ajudando a criação de elementos do lado
+ * do código cliente (código da aplicação), ou seja, quem for utilizar algum builder em alguma parte do app.
+ * Com ele, conseguimos remover chamadas chains repetidas (que semprem seguem o mesmo padrão) pela aplicação.
  */
 public class Director {
 
